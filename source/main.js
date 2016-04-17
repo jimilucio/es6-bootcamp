d6fd59271af662896ecaaf8bb680842d43778752
Luciano Murruni <luciano.murruni@gmail.com>
2016-04-17 09:02:12 +0200

    Change the import of translate with only one expression

--- source/main.js
+++ source/main.js
@@ -3,10 +3,8 @@ import Rx from 'rx';
 import componentHandler from 'material-design-lite';
 import './assets/es6-bootcamp.scss';
 import TextAnalyzer from './classes/TextAnalyzer';
-import Translater from './classes/Translater';
+import {default as Translater, ITALIAN, ENGLISH}  from './classes/Translater';
 import DomElementNavigation from './classes/DomElementNavigation';
-
-import { ITALIAN, ENGLISH } from './classes/Translater';
 
 const subTitleObject = document.getElementById('application-subtitle');
 const textarea = document.getElementById('application-text');
@@ -28,31 +26,31 @@ const analyzer = new TextAnalyzer();
 const translater = new Translater('trnsl.1.1.20160411T154306Z.c67c417ede790dc2.6616780d6aa5762f9c7ccd94bacde5e0cae37e54');
 
 function mapRadioToLanguage(radioValue) {
-  switch (radioValue) {
-    case '1':
-      return { from: ITALIAN, to: ENGLISH };
-    case '2':
-      return { from: ENGLISH, to: ITALIAN };
-    default:
-      return null;
-  }
+    switch (radioValue) {
+        case '1':
+            return {from: ITALIAN, to: ENGLISH};
+        case '2':
+            return {from: ENGLISH, to: ITALIAN};
+        default:
+            return null;
+    }
 }
 
 function updateStatistics(analyzer) {
-  textLength.innerText = analyzer.textLength(); // Prima
-  textLengthNoSpaces.innerText = analyzer.textLengthNoSpaces(); // Prima
-  sentencesCount.innerText = analyzer.sentences().length; // Prima
-  singularWordCount.innerText = analyzer.singularWords().size; // Seconda
-  singuarChars.innerText = analyzer.singularCharacters().size; // Seconda
-  averageWordLength.innerText = analyzer.averageWordLength(); // Seconda
-  mostUsedWords.innerText = analyzer.wordsByFrequence() // Seconda
-    .slice(0, 3)
-    .reduce((prev, curr) => `${prev} ${curr.word}`, '');
+    textLength.innerText = analyzer.textLength(); // Prima
+    textLengthNoSpaces.innerText = analyzer.textLengthNoSpaces(); // Prima
+    sentencesCount.innerText = analyzer.sentences().length; // Prima
+    singularWordCount.innerText = analyzer.singularWords().size; // Seconda
+    singuarChars.innerText = analyzer.singularCharacters().size; // Seconda
+    averageWordLength.innerText = analyzer.averageWordLength(); // Seconda
+    mostUsedWords.innerText = analyzer.wordsByFrequence() // Seconda
+      .slice(0, 3)
+      .reduce((prev, curr) => `${prev} ${curr.word}`, '');
 }
 
 noTranslate.addEventListener('click', event => {
-  textareaTranslated.disabled = true;
-  textareaTranslated.value = '';
+    textareaTranslated.disabled = true;
+    textareaTranslated.value = '';
 });
 
 const userInput = Rx.Observable.fromEvent(textarea, 'keydown')
@@ -60,22 +58,22 @@ const userInput = Rx.Observable.fromEvent(textarea, 'keydown')
   .map(event => event.target.value);
 
 const translateChange = userInput
-  .filter(value => document.querySelector('input[name="options"]:checked').value != 3)
+  .filter(value => document.querySelector('input[name="options"]:checked').value != 3);
 
 translateChange.subscribe(
   value => {
-    const { from, to } = mapRadioToLanguage(document.querySelector('input[name="options"]:checked').value);
-    textareaTranslated.disabled = false;
-    translater.translate(value, from, to)
-      .then(translated => textareaTranslated.value = translated);
+      const {from, to} = mapRadioToLanguage(document.querySelector('input[name="options"]:checked').value);
+      textareaTranslated.disabled = false;
+      translater.translate(value, from, to)
+        .then(translated => textareaTranslated.value = translated);
   },
   err => console.error(err)
 );
 
 userInput.subscribe(
   value => {
-    analyzer.setText(value);
-    updateStatistics(analyzer);
+      analyzer.setText(value);
+      updateStatistics(analyzer);
   },
   err => console.error(err)
 );
