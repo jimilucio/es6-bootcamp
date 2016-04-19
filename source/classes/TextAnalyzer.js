92c77ae32b82355fd4d0758ca04d160a149f7124
Luciano Murruni <luciano.murruni@gmail.com>
2016-04-19 23:03:21 +0200

    File esercizio 2

--- source/classes/TextAnalyzer.js
+++ source/classes/TextAnalyzer.js
@@ -1,5 +1,5 @@
 function removeEmptyElementsFromArray(prev, curr) {
-  return curr ? [...prev, curr] : [...prev];
+    return curr ? [...prev, curr] : [...prev];
 }
 
 /**
@@ -18,7 +18,7 @@ class TextAnalyzer {
    * @param {string} text is a string to store inside the class.
    */
   constructor(text = '') {
-    this.setText(text);
+      this.setText(text);
   }
 
   /**
@@ -26,7 +26,7 @@ class TextAnalyzer {
    * @param {string} text Text.
    */
   setText(text) {
-    this.text = text;
+      this.text = text;
   }
 
   /**
@@ -34,7 +34,7 @@ class TextAnalyzer {
    * @return {string} The current text.
    */
   getText() {
-    return this.text;
+      return this.text;
   }
 
   /**
@@ -42,7 +42,7 @@ class TextAnalyzer {
   * @return {int} Text length.
   */
   textLength() {
-    return this.text.length;
+      return this.text.length;
   }
 
   /**
@@ -50,8 +50,8 @@ class TextAnalyzer {
   * @return {int} Text without spaces.
   */
   textLengthNoSpaces() {
-    return this.wordsNoSpaces()
-      .reduce((prev, current) => prev + current.length, 0);
+      return this.wordsNoSpaces()
+        .reduce((prev, current) => prev + current.length, 0);
   }
 
   /**
@@ -59,9 +59,9 @@ class TextAnalyzer {
   * @return {array[string]} Words array.
   */
   words() {
-    return this.text.split(/[ \n\t]+/)
-      .map(word => word.trim())
-      .map(word => word.toLowerCase());
+      return this.text.split(/[ \n\t]+/)
+        .map(word => word.trim())
+        .map(word => word.toLowerCase());
   }
 
   /**
@@ -69,10 +69,10 @@ class TextAnalyzer {
   * @return {array[string]} Sentences.
   */
   sentences() {
-    return this.text
-      .split(/[.?!]+/)
-      .map(word => word.trim())
-      .reduce(removeEmptyElementsFromArray, []);
+      return this.text
+        .split(/[.?!]+/)
+        .map(word => word.trim())
+        .reduce(removeEmptyElementsFromArray, []);
   }
 
   /**
@@ -80,8 +80,8 @@ class TextAnalyzer {
   * @return {array[string]} Words array.
   */
   wordsNoSpaces() {
-    return this.words()
-      .reduce(removeEmptyElementsFromArray, []);
+      return this.words()
+        .reduce(removeEmptyElementsFromArray, []);
   }
 
   /**
@@ -89,8 +89,8 @@ class TextAnalyzer {
   * @return {array[string]} Words array.
   */
   wordsNoSpacesNoSymbols() {
-    return this.wordsNoSpaces()
-      .reduce((prev, curr) => [...prev, curr.replace(/[^a-zA-Z\d' ]/g, '')], []);
+      return this.wordsNoSpaces()
+        .reduce((prev, curr) => [...prev, curr.replace(/[^a-zA-Z\d' ]/g, '')], []);
   }
 
   //
@@ -101,51 +101,51 @@ class TextAnalyzer {
   * Returns a Map containing words and their frequencies.
   * @return {map} A Map {word => wordFrequence}.
   */
-  wordsMap() {
-    // If the word is already in the Map, increment its frequence, otherwise add it to the Map
-    return this.wordsNoSpacesNoSymbols()
-      .reduce((prev, curr) => prev.get(curr) ?
-        prev.set(curr, prev.get(curr) + 1) : prev.set(curr, 1), new Map());
-  }
+  // wordsMap() {
+  //   // If the word is already in the Map, increment its frequence, otherwise add it to the Map
+  //   return this.wordsNoSpacesNoSymbols()
+  //     .reduce((prev, curr) => prev.get(curr) ?
+  //       prev.set(curr, prev.get(curr) + 1) : prev.set(curr, 1), new Map());
+  // }
 
   /**
   * The words contained in the text, not repeated.
   * @return {set} A Set containing the singular words.
   */
-  singularWords() {
-    return new Set(this.wordsNoSpacesNoSymbols());
-  }
+  // singularWords() {
+  //   return new Set(this.wordsNoSpacesNoSymbols());
+  // }
 
   /**
   * The letters contained in the text.
   * @return {set} A set containing the singular characters.
   */
-  singularCharacters() {
-    return new Set(this.text);
-  }
+  // singularCharacters() {
+  //   return new Set(this.text);
+  // }
 
   /**
   * The words sorted by their frequency.
   * @return {array[object]} An array of {word, count} objects.
   */
-  wordsByFrequence() {
-    return [...this.wordsMap()]
-      .sort((a, b) => {
-        if (a[1] < b[1]) return 1;
-        else if (a[1] > b[1]) return -1;
-        else return 0;
-      })
-      .reduce((prev, curr) => [...prev, {word: curr[0], count: curr[1]}], []);
-  }
+  // wordsByFrequence() {
+  //   return [...this.wordsMap()]
+  //     .sort((a, b) => {
+  //       if (a[1] < b[1]) return 1;
+  //       else if (a[1] > b[1]) return -1;
+  //       else return 0;
+  //     })
+  //     .reduce((prev, curr) => [...prev, {word: curr[0], count: curr[1]}], []);
+  // }
 
   /**
   * The average length of the words.
   * @return {int}
   */
-  averageWordLength() {
-    const average = this.textLengthNoSpaces() / this.wordsNoSpaces().length;
-    return average ? Math.round(average) : 0;
-  }
+  // averageWordLength() {
+  //   const average = this.textLengthNoSpaces() / this.wordsNoSpaces().length;
+  //   return average ? Math.round(average) : 0;
+  // }
 
 }
 
--- source/main.js
+++ source/main.js
@@ -3,73 +3,35 @@ import Rx from 'rx';
 import componentHandler from 'material-design-lite';
 import './assets/es6-bootcamp.scss';
 import TextAnalyzer from './classes/TextAnalyzer';
-import {default as Translater, ITALIAN, ENGLISH}  from './classes/Translater';
 import DomElementNavigation from './classes/DomElementNavigation';
 
-const subTitleObject = document.getElementById('application-subtitle');
+//set the dom element costants
+//textarea
 const textarea = document.getElementById('application-text');
-const textareaTranslated = document.getElementById('application-text-translated');
 
+//information on the right
 const textLength = document.getElementById('text-length');
 const textLengthNoSpaces = document.getElementById('text-length-nospaces');
-const singularWordCount = document.getElementById('singular-words');
-const singuarChars = document.getElementById('singular-chars');
-const averageWordLength = document.getElementById('average-word-length');
 const sentencesCount = document.getElementById('sentences-count');
-const mostUsedWords = document.getElementById('most-used-words');
-
-const noTranslate = document.querySelector('input[name="options"][value="3"]');
 
 const navigationButton = new DomElementNavigation('.mdl-button');
 
 const analyzer = new TextAnalyzer();
-const translater = new Translater('trnsl.1.1.20160411T154306Z.c67c417ede790dc2.6616780d6aa5762f9c7ccd94bacde5e0cae37e54');
 
-function mapRadioToLanguage(radioValue) {
-    switch (radioValue) {
-        case '1':
-            return {from: ITALIAN, to: ENGLISH};
-        case '2':
-            return {from: ENGLISH, to: ITALIAN};
-        default:
-            return null;
-    }
-}
-
+//this function provide to update the information of the content on the right
 function updateStatistics(analyzer) {
     textLength.innerText = analyzer.textLength(); // Prima
     textLengthNoSpaces.innerText = analyzer.textLengthNoSpaces(); // Prima
     sentencesCount.innerText = analyzer.sentences().length; // Prima
-    singularWordCount.innerText = analyzer.singularWords().size; // Seconda
-    singuarChars.innerText = analyzer.singularCharacters().size; // Seconda
-    averageWordLength.innerText = analyzer.averageWordLength(); // Seconda
-    mostUsedWords.innerText = analyzer.wordsByFrequence() // Seconda
-      .slice(0, 3)
-      .reduce((prev, curr) => `${prev} ${curr.word}`, '');
+
 }
 
-noTranslate.addEventListener('click', event => {
-    textareaTranslated.disabled = true;
-    textareaTranslated.value = '';
-});
-
+// RX.jS on keydown send the new event to the subscribers
 const userInput = Rx.Observable.fromEvent(textarea, 'keydown')
   .debounce(50)
   .map(event => event.target.value);
 
-const translateChange = userInput
-  .filter(value => document.querySelector('input[name="options"]:checked').value != 3);
-
-translateChange.subscribe(
-  value => {
-      const {from, to} = mapRadioToLanguage(document.querySelector('input[name="options"]:checked').value);
-      textareaTranslated.disabled = false;
-      translater.translate(value, from, to)
-        .then(translated => textareaTranslated.value = translated);
-  },
-  err => console.error(err)
-);
-
+// on event update statistics
 userInput.subscribe(
   value => {
       analyzer.setText(value);
