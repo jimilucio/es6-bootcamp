2f72298247488e7b792a4580700f086bc469ab32
Luciano Murruni <luciano.murruni@gmail.com>
2016-07-01 18:21:33 +0200

    part2 removed

--- source/classes/TextAnalyzer.js
+++ source/classes/TextAnalyzer.js
@@ -93,60 +93,6 @@ class TextAnalyzer {
         .reduce((prev, curr) => [...prev, curr.replace(/[^a-zA-Z\d' ]/g, '')], []);
   }
 
-  //
-  // Seconda parte
-  //
-
-  /**
-  * Returns a Map containing words and their frequencies.
-  * @return {map} A Map {word => wordFrequence}.
-  */
-  // wordsMap() {
-  //   // If the word is already in the Map, increment its frequence, otherwise add it to the Map
-  //   return this.wordsNoSpacesNoSymbols()
-  //     .reduce((prev, curr) => prev.get(curr) ?
-  //       prev.set(curr, prev.get(curr) + 1) : prev.set(curr, 1), new Map());
-  // }
-
-  /**
-  * The words contained in the text, not repeated.
-  * @return {set} A Set containing the singular words.
-  */
-  // singularWords() {
-  //   return new Set(this.wordsNoSpacesNoSymbols());
-  // }
-
-  /**
-  * The letters contained in the text.
-  * @return {set} A set containing the singular characters.
-  */
-  // singularCharacters() {
-  //   return new Set(this.text);
-  // }
-
-  /**
-  * The words sorted by their frequency.
-  * @return {array[object]} An array of {word, count} objects.
-  */
-  // wordsByFrequence() {
-  //   return [...this.wordsMap()]
-  //     .sort((a, b) => {
-  //       if (a[1] < b[1]) return 1;
-  //       else if (a[1] > b[1]) return -1;
-  //       else return 0;
-  //     })
-  //     .reduce((prev, curr) => [...prev, {word: curr[0], count: curr[1]}], []);
-  // }
-
-  /**
-  * The average length of the words.
-  * @return {int}
-  */
-  // averageWordLength() {
-  //   const average = this.textLengthNoSpaces() / this.wordsNoSpaces().length;
-  //   return average ? Math.round(average) : 0;
-  // }
-
 }
 
 export default TextAnalyzer;
